8f2af4791458625070a911b9914585ab0977db8a
Shane Kilkelly <shane@kilkelly.me>
2018-06-17 18:40:28 +0100

    Add a when_null option

--- lib/hammer_plug.ex
+++ lib/hammer_plug.ex
@@ -17,6 +17,7 @@ defmodule Hammer.Plug do
     scale = Keyword.get(opts, :scale, 60_000)
     limit = Keyword.get(opts, :limit, 60)
     by = Keyword.get(opts, :by, :ip)
+    when_nil = Keyword.get(opts, :when_nil, :use_nil)
 
     on_deny_handler =
       Keyword.get(
@@ -26,10 +27,57 @@ defmodule Hammer.Plug do
       )
 
     if !is_valid_method(by) do
-      raise "Hammer.Plug: invalid `by` parameter: #{to_string(by)}"
+      raise "Hammer.Plug: invalid `by` parameter"
     end
 
-    full_id = build_identifier(conn, id_prefix, by)
+    request_identifier = get_request_identifier(conn, by)
+
+    case request_identifier do
+      nil ->
+        case when_nil do
+          # Proceed
+          :use_nil ->
+            do_check(conn, id_prefix, nil, scale, limit, on_deny_handler)
+
+          :raise ->
+            by_string = by_to_string(by)
+            raise "Hammer.Plug: request-id value is nil, by: #{by_string}"
+
+          :pass ->
+            # Skip check
+            conn
+        end
+
+      id ->
+        do_check(conn, id_prefix, id, scale, limit, on_deny_handler)
+    end
+  end
+
+  def default_on_deny_handler(conn, _opts) do
+    conn
+    |> send_resp(429, "Too Many Requests")
+    |> halt()
+  end
+
+  ## Private helpers
+
+  defp get_request_identifier(conn, by) do
+    case by do
+      :ip ->
+        conn.remote_ip
+        |> Tuple.to_list()
+        |> Enum.join(".")
+
+      {:session, key} ->
+        get_session(conn, key)
+
+      {:session, key, func} ->
+        get_session(conn, key) |> func.()
+    end
+  end
+
+  defp do_check(conn, id_prefix, request_id, scale, limit, on_deny_handler) do
+    full_id = "#{id_prefix}:#{request_id}"
 
     case Hammer.check_rate(full_id, scale, limit) do
       {:allow, _n} ->
@@ -40,10 +88,12 @@ defmodule Hammer.Plug do
     end
   end
 
-  def default_on_deny_handler(conn, _opts) do
-    conn
-    |> send_resp(429, "Too Many Requests")
-    |> halt()
+  defp by_to_string(by) do
+    case by do
+      a when is_atom(a) -> to_string(a)
+      {:session, key} -> "{:session, #{key}}"
+      {:session, key, _func} -> "{:session, #{key}, &fn}"
+    end
   end
 
   defp is_valid_method(by) do
@@ -53,24 +103,5 @@ defmodule Hammer.Plug do
       {:session, key, func} when is_atom(key) and is_function(func) -> true
       _ -> false
     end
-  end
-
-  defp build_identifier(conn, prefix, :ip) do
-    ip_string =
-      conn.remote_ip
-      |> Tuple.to_list()
-      |> Enum.join(".")
-
-    "#{prefix}:#{ip_string}"
-  end
-
-  defp build_identifier(conn, prefix, {:session, key}) do
-    session_val = get_session(conn, key)
-    "#{prefix}:#{session_val}"
-  end
-
-  defp build_identifier(conn, prefix, {:session, key, func}) do
-    session_val = get_session(conn, key) |> func.()
-    "#{prefix}:#{session_val}"
   end
 end
