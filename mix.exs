af27a24574c411e3f93c570eea80229f06fe5d6c
Shane Kilkelly <shane@kilkelly.me>
2018-06-23 11:36:51 +0100

    Bump to version 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hammer.Plug.MixProject do
   def project do
     [
       app: :hammer_plug,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
