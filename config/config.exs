a248b56b6fd3c84ac3be89bd3300bd834598cc59
Shane Kilkelly <shane@kilkelly.me>
2019-07-21 13:27:06 +0100

    Remove config

--- config/config.exs
+++ config/config.exs
@@ -2,9 +2,6 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :hammer,
-  backend: {Hammer.Backend.ETS, expiry_ms: 3_600_000, cleanup_interval_ms: 3_600_000}
-
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
